62b83042b7f58224964b1533bd55e10877d1465d
Luke Bayes <lukebayes@gmail.com>
2021-04-29 12:57:10 -0400

    Updated build script

--- cee/build.sh
+++ cee/build.sh
@@ -2,15 +2,20 @@
 
 build_type="$1"
 cmake_build_type="release"
+target=""
 
 if [ -z "$1" ]; then
   build_type="release"
 fi
 
-if [ "$1" -eq "test" ]; then
+if [ "$1" == "test" ]; then
   cmake_build_type="Debug"
-elif [ "$1" -eq "debug" ]; then
+elif [ "$1" == "debug" ]; then
   cmake_build_type="Debug"
+elif [ "$1" == "gtk" ]; then
+  cmake_build_type="Debug"
+  build_type="debug"
+  target="maple-gtk"
 else
   cmake_build_type="Release"
 fi
@@ -25,10 +30,14 @@ pushd "cmake-build-$build_type"
 
 make clean
 echo "Building sources"
-make
-if [ $build_type = "test" ]; then
+make $target
+
+if [ $build_type == "test" ]; then
   echo "Running tests"
   ./maple-test
+elif [ $target == "maple-gtk" ]; then
+  echo "Running GTK app"
+  ./$target
 fi
 popd
 echo "Build complete, exiting"
