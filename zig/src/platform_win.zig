cd284079d21f11841f4495e637f73184dfeeb793
Luke Bayes <lukebayes@gmail.com>
2021-05-25 17:07:36 -0400

    Showing device strings for windows

--- zig/src/platform_win.zig
+++ zig/src/platform_win.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const win32 = @import("win32");
 
+const fmt = std.fmt;
+const mem = std.mem;
 const print = std.debug.print;
 
 usingnamespace win32.media.audio.core_audio;
@@ -11,10 +13,30 @@ usingnamespace win32.system.properties_system;
 usingnamespace win32.system.system_services;
 usingnamespace win32.zig;
 
-const DEFAULT_DEVICE_NAME = "[unknown]";
+// const DEFAULT_DEVICE_NAME: []u8 = "[unknown]";
+
+fn printU16String(ptr: [*:0]const u16) []u8 {
+    const len = mem.len(ptr);
+    var result: [256]u8 = undefined;
+
+    var smaller = result[0 .. len + 1];
+
+    print("THE GODDAMN UTF16 STRING is: ", .{}); // in next lines
+    var i: u16 = 0;
+    while (i < len) : (i += 1) {
+        print("{c}", .{@truncate(u8, ptr[i])});
+        smaller[i] = @truncate(u8, ptr[i]);
+    }
+    smaller[i] = @as(u8, 0);
+
+    print("\n", .{});
+    print("SMALLER: {s}\n", .{smaller});
+
+    return smaller;
+}
 
 pub const AudioDevice = struct {
-    name: []const u8 = DEFAULT_DEVICE_NAME,
+    name: []const u8 = "[unknown]",
 };
 
 pub fn info() []const u8 {
@@ -70,61 +92,116 @@ pub const AudioApi = struct {
         print("---------------------\n", .{});
         print("default device: {s}\n", .{device});
 
-        var audio_client: *IAudioClient = undefined;
+        var device_id: PWSTR = undefined;
         {
-            const status = device.IMMDevice_Activate(
-                IID_IAudioClient,
-                @enumToInt(CLSCTX_ALL),
-                null,
-                @ptrCast(**c_void, &audio_client),
-            );
+            const status = device.IMMDevice_GetId(&device_id);
             if (FAILED(status)) {
-                print("AudioClient failed {d}\n", .{status});
+                print("Failed to get device id: {d}\n", .{status});
                 return error.Fail;
             }
         }
-        defer _ = audio_client.IUnknown_Release();
-        print("Audio Clien: {s}\n", .{audio_client});
+        print("--------------------\n", .{});
+        print("device_id: {d}\n", .{device_id});
+        print("device_id len: {d}\n", .{mem.len(device_id)});
 
-        // var properties: *IPropertyStore = undefined;
+        // var audio_client: *IAudioClient = undefined;
         // {
-        //     const status = device.IMMDevice_OpenPropertyStore(STGM_READ, &properties);
+        //     const status = device.IMMDevice_Activate(
+        //         IID_IAudioClient,
+        //         @enumToInt(CLSCTX_ALL),
+        //         null,
+        //         @ptrCast(**c_void, &audio_client),
+        //     );
         //     if (FAILED(status)) {
-        //         print("DEVICE PROPS: {d}\n", .{status});
+        //         print("AudioClient failed {d}\n", .{status});
         //         return error.Fail;
         //     }
         // }
-        // defer _ = properties.IUnknown_Release();
+        // defer _ = audio_client.IUnknown_Release();
+        // print("Audio Client: {s}\n", .{audio_client});
 
-        // var count: u32 = 0;
-        // {
-        //     const status = properties.IPropertyStore_GetCount(&count);
-        //     if (FAILED(status)) {
-        //         print("GetCount failed: {d}\n", .{status});
-        //         return error.Fail;
-        //     }
-        // }
+        var properties: *IPropertyStore = undefined;
+        {
+            const status = device.IMMDevice_OpenPropertyStore(STGM_READ, &properties);
+            if (FAILED(status)) {
+                print("DEVICE PROPS: {d}\n", .{status});
+                return error.Fail;
+            }
+        }
+        defer _ = properties.IUnknown_Release();
+        print("--------------\n", .{});
+        print("props: {s}\n", .{properties});
 
-        // var index: u32 = 0;
-        // while (index < count - 1) : (index += 1) {
-        //     var propKey: PROPERTYKEY = undefined;
-        //     var propValue: PROPVARIANT = undefined;
+        var friendly_name: PROPVARIANT = undefined;
+        var friendly_name_ptr: [*:0]u16 = undefined;
+        {
+            const status = properties.IPropertyStore_GetValue(&DEVPKEY_Device_FriendlyName, &friendly_name);
+            if (FAILED(status)) {
+                print("friendly_name failed: {d}\n", .{status});
+                return error.Fail;
+            }
 
-        //     print("index: {d}\n", .{index});
-        //     const p_status = properties.IPropertyStore_GetAt(index, &propKey);
-        //     if (FAILED(p_status)) {
-        //         print("Failed to getAt {x}\n", .{p_status});
-        //         return error.Fail;
-        //     }
+            var vt = friendly_name.Anonymous.Anonymous.vt;
+            // var ptr = &friendly_name.Anonymous.Anonymous.Anonymous.cVal;
+            // var ptr = friendly_name.Anonymous.Anonymous.Anonymous.pcVal;
+            // var ptr = &friendly_name.Anonymous.Anonymous.Anonymous.calpstr;
+            // var ptr = &friendly_name.Anonymous.Anonymous.Anonymous.calpwstr;
+            // var ptr = friendly_name.Anonymous.Anonymous.Anonymous.pszVal;
+            // var ptr = friendly_name.Anonymous.Anonymous.Anonymous.pwszVal;
+            friendly_name_ptr = friendly_name.Anonymous.Anonymous.Anonymous.pwszVal;
+            _ = printU16String(friendly_name_ptr);
+        }
 
-        //     print("Looping properties with: {s}\n", .{propKey});
-        //     const v_status = properties.IPropertyStore_GetValue(&propKey, &propValue);
-        //     print("Found value: {s}\n", .{propValue});
-        // }
+        // Enumerate over the prop store for each prop
+        var count: u32 = 0;
+        {
+            const status = properties.IPropertyStore_GetCount(&count);
+            if (FAILED(status)) {
+                print("GetCount failed: {d}\n", .{status});
+                return error.Fail;
+            }
+        }
+
+        print("-------------------------\n", .{});
+        var index: u32 = 0;
+        while (index < count - 1) : (index += 1) {
+            print("-----\n", .{});
+            var prop_key: PROPERTYKEY = undefined;
+            var prop_value: PROPVARIANT = undefined;
+
+            print("index: {d}\n", .{index});
+            const p_status = properties.IPropertyStore_GetAt(index, &prop_key);
+            if (FAILED(p_status)) {
+                print("Failed to getAt {x}\n", .{p_status});
+                return error.Fail;
+            }
+
+            print("Looping properties with: {s}\n", .{prop_key});
+            const v_status = properties.IPropertyStore_GetValue(&prop_key, &prop_value);
+
+            const vt = prop_value.Anonymous.Anonymous.vt;
+            print("Found value: {s}\n", .{prop_value});
+            if (31 == vt) {
+                const v_ptr = prop_value.Anonymous.Anonymous.Anonymous.pwszVal;
+                //print("crapper: {s}\n", .{v_ptr});
+                // const v_ptr = prop_value.Anonymous.Anonymous.Anonymous.pwszVal;
+                _ = printU16String(v_ptr);
+            } else {
+                print("vt NOT NOT NOT 31 - {d}\n", .{vt});
+            }
+        }
+        print("-------------------------\n", .{});
 
         // print("post device: {s}\n", .{device.IMMDevice_GetId()});
+        var foo = printU16String(friendly_name_ptr);
+        print("FOOOOOOOOOOOOOOOOOOOOOOOOO: {s}\n", .{foo});
 
-        return AudioDevice{};
+        var audio_device = AudioDevice{
+            .name = foo,
+        };
+        // mem.copy(u8, audio_device.name, friendly_name_ptr);
+
+        return audio_device;
     }
 };
 
