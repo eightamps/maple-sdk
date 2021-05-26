e12fd3daa9b42341525bf19a9643190f27922555
Luke Bayes <lukebayes@gmail.com>
2021-05-26 12:11:24 -0400

    hid_client is working on Windows VM
    Does not work under wine64, but likely because USB devices are not being
    replicated or made available to the virtual environment.
    
    This also requires manual duplication of .dll files from
    vendor/libusb-win32/VS2019/MS64/dll/libusb-1.0.* to dist/

--- zig/build.zig
+++ zig/build.zig
@@ -16,8 +16,11 @@ fn linkLibs(step: *std.build.LibExeObjStep, is_windows: bool, is_linux: bool) vo
     } else if (is_windows) {
         step.addIncludeDir("vendor/libusb-win32/include");
         step.addLibPath("vendor/libusb-win32/VS2019/MS64/dll");
+        // step.addLibPath("vendor/libusb-win32/MinGW64/static");
 
         step.linkSystemLibraryName("libusb-1.0");
+
+        // step.linkSystemLibrary("libusb-1.0");
         step.linkSystemLibrary("uuid");
         step.linkSystemLibrary("ole32");
         step.addPackage(.{
